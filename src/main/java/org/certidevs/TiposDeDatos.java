c34293054d36ac13ff811a1b42b17619f6bbcdd6
david de la puente enriquez <Twinkymen@hotmail.com>
2025-04-07 11:54:14 +0200

    Añadido archivo sobre tipos de datos de Alan.

---
+++ src/main/java/org/certidevs/TiposDeDatos.java
@@ -0,0 +1,23 @@
+package org.certidevs;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        // Comentario
+
+        // Crear una variable edad:
+
+        int edad = 25;
+        System.out.println(edad);
+
+        double altura = 1.60;
+
+        String nombre = "Alan";
+
+        System.out.print("Mi nombre es: " + nombre + " y mi altura es: " + altura);
+
+
+    }
+
+}
